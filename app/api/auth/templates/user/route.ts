ed4e9bdfe555274e9717d5f320e9a39881025616
nsfwbunny <bennialencar@gmail.com>
2025-11-11 00:03:55 -0300

    Fix type annotation for GET function parameter

--- app/api/auth/templates/user/route.ts
+++ app/api/auth/templates/user/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 
-export async function GET(req) {
+export async function GET(re: anyq) {
   // Exemplo com usuário fixo
   const user = await prisma.user.findFirst();
   return NextResponse.json({ user });
